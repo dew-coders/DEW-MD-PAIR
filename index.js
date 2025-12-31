2ad19c083e04cdc9ced487e5d50b937a46b687f5
Dew-Coders <mullertech001@gmail.com>
2025-12-31 19:12:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,21 +8,10 @@ let server = require('./qr'),
 require('events').EventEmitter.defaultMaxListeners = 500;
 app.use('/server', server);
 app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/qr',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Don't Forgot To Give Star
-
  Server running on http://localhost:` + PORT)
 })
 
